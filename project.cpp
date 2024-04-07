84d2d246b4d90e5cebf46109542611aeaa8a3f46
Kwokki <95371638+kwo-code@users.noreply.github.com>
2024-04-07 21:26:39 +0300

    Add files via upload

--- project.cpp
+++ project.cpp
@@ -3,13 +3,13 @@
 #include <ctime>
 #include <chrono>
 
-float getRandomFloat(float min, float max) {
-    return floor(min + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (max - min))));
+long getRandomlong(long min, long max) {
+    return floor(min + static_cast<long>(rand()) / (static_cast<long>(RAND_MAX / (max - min))));
 }
 
-void printArray(float arr[], int size, int hide) {
+void prlongArray(long arr[], long size, long hide) {
     if (hide != 1) {
-        for (int i = 0; i < size; ++i) {
+        for (long i = 0; i < size; ++i) {
         std::cout << arr[i] << " ";
     }
     std::cout << '\n';
@@ -17,18 +17,18 @@ void printArray(float arr[], int size, int hide) {
     
 }
 
-void copyArray(float arr[], float arrCopy[], int size) {
-    for (int i = 0; i < size; ++i) {
+void copyArray(long arr[], long arrCopy[], long size) {
+    for (long i = 0; i < size; ++i) {
         arrCopy[i] = arr[i];
     }
 }
 
 
-void bubbleSort(float arr[], int size) {
-    for (int i = 0; i < size - 1; ++i) {
-        for (int j = 0; j < size - i - 1; ++j) {
+void bubbleSort(long arr[], long size) {
+    for (long i = 0; i < size - 1; ++i) {
+        for (long j = 0; j < size - i - 1; ++j) {
             if (arr[j] > arr[j + 1]) {
-                float temp = arr[j];
+                long temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
@@ -36,10 +36,10 @@ void bubbleSort(float arr[], int size) {
     }
 }
 
-void insertsSort(float arr[], int size) {
-    for (int i = 1; i < size; ++i) {
-        int value = arr[i];
-        int position = i;
+void insertsSort(long arr[], long size) {
+    for (long i = 1; i < size; ++i) {
+        long value = arr[i];
+        long position = i;
         while (position > 0 && arr[position - 1] > value) {
             arr[position] = arr[position - 1];
             position--;
@@ -49,113 +49,113 @@ void insertsSort(float arr[], int size) {
     }
 }
 
-void choiseSort(float arr[], int size) {
-    for (int i = 0; i < size-1; ++i){ 
-        int min = i;
-        for (int x = i+1; x < size; ++x) {
+void choiseSort(long arr[], long size) {
+    for (long i = 0; i < size-1; ++i){ 
+        long min = i;
+        for (long x = i+1; x < size; ++x) {
             if (arr[x] < arr[min]) {
                 min = x;
             }
         }
         if (min != i){
-            float temp = arr[i];
+            long temp = arr[i];
             arr[i] = arr[min];
             arr[min] = temp;
         }
     }
 }
 
-void shellSort(float arr[], int size) {
-    for (int interval = size / 2; interval > 0; interval /= 2) {
-        for (int i = interval; i < size; i++) {
-            int temp = arr[i];
-            int j;
-            for (j = i; j >= interval && arr[j - interval] > temp; j -= interval) {
-                arr[j] = arr[j - interval];
+void shellSort(long arr[], long size) {
+    for (long longerval = size / 2; longerval > 0; longerval /= 2) {
+        for (long i = longerval; i < size; i++) {
+            long temp = arr[i];
+            long j;
+            for (j = i; j >= longerval && arr[j - longerval] > temp; j -= longerval) {
+                arr[j] = arr[j - longerval];
             }
             arr[j] = temp;
         }
     }
 }
 
-int partition(float arr[], int low, int high) {
-    float pivot = arr[high];
-    int i = low - 1;
+long partition(long arr[], long low, long high) {
+    long pivot = arr[high];
+    long i = low - 1;
 
-    for (int j = low; j < high; j++) {
+    for (long j = low; j < high; j++) {
         if (arr[j] <= pivot) {
             i++;
-            float temp = arr[i];
+            long temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
         }
     }
-    float temp = arr[i + 1];
+    long temp = arr[i + 1];
     arr[i + 1] = arr[high];
     arr[high] = temp;
     return i + 1;
 }
 
-void quickSort(float arr[], int low, int high) {
+void quickSort(long arr[], long low, long high) {
     if (low < high) {
-        int pi = partition(arr, low, high);
+        long pi = partition(arr, low, high);
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
 
-int main() {
-    int size;
+long main() {
+    long size;
     int hide;
     std::cout << "Enter size of array: ";
     std::cin >> size;
     std::cout << "Hide arr? 1/0: ";
     std::cin >> hide;
 
-    float *arr = new float[size];
-    float *arrCopy = new float[size];
-    for (int i = 0; i < size; ++i) {
-        arr[i] = getRandomFloat(-1000.0f, 1000.0f);
+    long *arr = new long[size];
+    long *arrCopy = new long[size];
+    for (long i = 0; i < size; ++i) {
+        arr[i] = getRandomlong(-1000.0f, 1000.0f);
     }
 
     if (hide != 1) std::cout << "Source array:\n";
     
-    printArray(arr, size, hide);
+    prlongArray(arr, size, hide);
 
     copyArray(arr, arrCopy, size);
     auto start = std::chrono::steady_clock::now();
     bubbleSort(arrCopy, size);
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start); 
     std::cout << "\nBubble sorting:\n Time: " << duration.count() << " microsec" << std::endl;
-    printArray(arrCopy, size, hide);
+    prlongArray(arrCopy, size, hide);
 
     copyArray(arr, arrCopy, size);
     start = std::chrono::steady_clock::now();
     insertsSort(arrCopy, size);
     duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start); 
     std::cout << "\nInsert sorting:\n Time: " << duration.count() << " microsec" << std::endl;
-    printArray(arrCopy, size, hide);
+    prlongArray(arrCopy, size, hide);
 
     copyArray(arr, arrCopy, size);
     start = std::chrono::steady_clock::now();
     choiseSort(arrCopy, size);
     duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start); 
     std::cout << "\nChoise sorting:\n Time: " << duration.count() << " microsec" << std::endl;
-    printArray(arrCopy, size, hide);
+    prlongArray(arrCopy, size, hide);
 
     copyArray(arr, arrCopy, size);
     start = std::chrono::steady_clock::now();
     shellSort(arrCopy, size);
     duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start); 
     std::cout << "\nShell sorting:\n Time: " << duration.count() << " microsec" << std::endl;
-    printArray(arrCopy, size, hide);
+    prlongArray(arrCopy, size, hide);
 
     copyArray(arr, arrCopy, size);
     start = std::chrono::steady_clock::now();
     quickSort(arrCopy, 0, size-1);
     duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start); 
     std::cout << "\nQuick sorting:\n Time: " << duration.count() << " microsec" << std::endl;
-    printArray(arrCopy, size, hide);
+    prlongArray(arrCopy, size, hide);
     system("pause");
     return 0;
 }
